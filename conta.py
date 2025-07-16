6214d628f0a0bbd149ba860ae641fe6d8dbe3812
Alison Bruno <161540994+ABMS-NS@users.noreply.github.com>
2025-07-16 12:35:41 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,15 +1,34 @@
 
+class metas_investimento:
+    def __init__(self, titulo, valor):
+        self.titulo = titulo
+        self.valor_alvo = valor
+        self.valor_atual = 0
+
+    def depositar_na_meta(self, valor, conta):
+        self.valor_atual += valor
+        conta.sacar(valor)
+        conta.historico.append("Depositado na meta.")
+
+        if self.valor_atual <= self.valor_alvo:
+            print("Você atingiu a meta!")
+        
+
+
+
 
 
 
 class Conta:
     #toda vez que criar um novo usuário precisa chamar o construtor
-    def __init__(self, nome, senha, saldo_inicial):
+    def __init__(self, nome, senha, saldo_inicial, talao_cheque):
         self.nome = nome
         self.senha = senha
         self.saldo = saldo_inicial
         self.saldo_dolar = 0.0
         self.historico = []
+        self.talao_cheque = talao_cheque
+        self.metas = []
 
     #não está nos requisitos mas coloquei por uma boa prática
     def sacar(self, valor):
@@ -30,3 +49,25 @@ class Conta:
     def consultar_saldo_dolar(self):
         return self.saldo_dolar
     
+    def verificar_talao_cheque(self):
+        if self.talao_cheque == 0:
+            print("Você não pediu um talão de cheque esse mês.")
+            
+            resposta = input("Deseja adquirir um? (s/n): ").strip().lower()
+            if resposta == 's':
+                self.talao_cheque = 1
+                print("Talão de cheque adquirido com sucesso!")
+                self.historico.append("Talão de cheque adquirido.")
+            else:
+                print("Você optou por não adquirir um talão de cheque.")
+
+        else:
+            print("Você já pediu um talão de cheque esse mês.")
+            resposta = input("Deseja solicitar outro? (s/n): ").strip().lower()
+            if resposta == 's':
+                self.sacar(10)  #custo de R$10 para solicitar um novo talão
+                self.talao_cheque += 1
+                print("Novo talão de cheque solicitado com sucesso!")
+                self.historico.append("Talão de cheque adquirido.")
+            else: 
+                print("Você optou por não solicitar outro talão de cheque.")
